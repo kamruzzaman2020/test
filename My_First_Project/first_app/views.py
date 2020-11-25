e4f5ca6c4e4ab7e00b98d6289d722fca03831ec2
Kamruzzaman Mithu <kamruzzaman.mithu@outlook.com>
2020-11-26 01:32:23 +0600

    First commit

---
+++ My_First_Project/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    diction = {'text_1': 'I am a text sent from views.py'}
+    return render(request, 'first_app/index.html', context=diction)
